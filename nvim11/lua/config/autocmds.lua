b4530f0a31ce898a8440f9ed10f748a4a05e53d0
Masato Takatani <takatani.masato@gmail.com>
2025-10-10 09:18:10 -0600

    removed nvim11

--- nvim11/lua/config/autocmds.lua
+++
@@ -1,9 +0,0 @@
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "make",
-  callback = function()
-    vim.opt_local.expandtab = false
-    vim.opt_local.tabstop = 8
-    vim.opt_local.shiftwidth = 8
-  end,
-})
-
--- nvim11/lua/plugins/lsp.lua
+++
@@ -1,7 +0,0 @@
-return {
-  -- Neovim 0.10 以降は nvim-lspconfig 不要！
-  -- 組み込みの vim.lsp.config API を使用
-  "neovim/nvim-lspconfig",  -- ※削除してもOK（依存プラグインで使う場合は残してもよい）
-  enabled = false,  -- lazy.nvim に読み込ませないように
-}
-
