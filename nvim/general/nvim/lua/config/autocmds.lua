e7ab342188d1b9bec9a0d3e1233543367eb1a86b
Masato Takatani <takatani.masato@gmail.com>
2025-10-13 21:21:44 -0600

    nvim general

---
+++ nvim/general/nvim/lua/config/autocmds.lua
@@ -0,0 +1,17 @@
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "make",
+  callback = function()
+    vim.opt_local.expandtab = false
+    vim.opt_local.tabstop = 8
+    vim.opt_local.shiftwidth = 8
+  end,
+})
+
+-- Highlight when yanking
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
