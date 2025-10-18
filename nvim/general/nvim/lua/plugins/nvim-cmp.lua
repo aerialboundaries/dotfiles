871e00758021cf0a4fea263e5edac637465a3c0b
Masato Takatani <takatani.masato@gmail.com>
2025-10-18 00:03:44 -0600

    changed nvim-cmp

--- nvim/general/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/general/nvim/lua/plugins/nvim-cmp.lua
@@ -65,11 +65,11 @@ return { -- Autocompletion
 				-- Accept ([y]es) the completion.
 				--  This will auto-import if your LSP supports it.
 				--  This will expand snippets if the LSP sent a snippet.
-				["<C-y>"] = cmp.mapping.confirm({ select = true }),
+				["<C-y>"] = cmp.mapping.confirm({ select = false }), --最初の項目を自動で選択しない
 
 				-- If you prefer more traditional completion keymaps,
 				-- you can uncomment the following lines
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
+				["<CR>"] = cmp.mapping.confirm({ select = false }), -- 最初の項目を自動で選択しない
 				["<Tab>"] = cmp.mapping.select_next_item(),
 				["<S-Tab>"] = cmp.mapping.select_prev_item(),
 
