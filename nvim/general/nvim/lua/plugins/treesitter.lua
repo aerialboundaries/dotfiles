39b9b94d55cf7cf726ab3456b0fb309f92f0f0ea
Masato Takatani <takatani.masato@gmail.com>
2025-10-21 01:44:47 -0600

    markdown

--- nvim/general/nvim/lua/plugins/treesitter.lua
+++ nvim/general/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
 	build = ":TSUpdate",
 	config = function()
 		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "c", "cpp", "lua", "python", "bash" },
+			ensure_installed = { "c", "cpp", "lua", "python", "bash", "markdown", "markdown_inline" },
 			highlight = { enable = true },
 		})
 	end,
