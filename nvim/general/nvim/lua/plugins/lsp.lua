144339776c46c1e5ddf37d0b05818f8dab350175
Masato Takatani <takatani.masato@gmail.com>
2025-10-26 02:26:29 -0600

    from rz57

--- nvim/general/nvim/lua/plugins/lsp.lua
+++ nvim/general/nvim/lua/plugins/lsp.lua
@@ -227,6 +227,7 @@ return {
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
 				"stylua", -- Used to format Lua code
+				"codelldb", -- ✨ C/C++ デバッグアダプターの自動インストールを追加
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
