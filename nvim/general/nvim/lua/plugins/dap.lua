5551706edd7d7b92fcf12f94912c9b38c9297caf
Masato Takatani <takatani.masato@gmail.com>
2025-10-26 03:22:08 -0600

    dap setting

--- nvim/general/nvim/lua/plugins/dap.lua
+++ nvim/general/nvim/lua/plugins/dap.lua
@@ -1,30 +1,29 @@
 return {
-  "mfussenegger/nvim-dap",
-  dependencies = {
-    "rcarriga/nvim-dap-ui",
-    "nvim-neotest/nvim-nio",
-  },
-  config = function()
-    local dap = require("dap")
-    local dapui = require("dapui")
-    dapui.setup()
-    dap.adapters.cppdbg = {
-      type = "executable",
-      command = "lldb-vscode",
-      name = "lldb",
-    }
-    dap.configurations.cpp = {
-      {
-        name = "Launch file",
-        type = "cppdbg",
-        request = "launch",
-        program = function()
-          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-        end,
-        cwd = "${workspaceFolder}",
-        stopAtEntry = true,
-      },
-    }
-  end,
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"nvim-neotest/nvim-nio",
+	},
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
+		dapui.setup()
+		dap.adapters.codellb = {
+			type = "executable",
+			command = "codelldb",
+			name = "lldb",
+		}
+		dap.configurations.cpp = {
+			{
+				name = "Launch file",
+				type = "codelldb",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+				stopAtEntry = true,
+			},
+		}
+	end,
 }
-
